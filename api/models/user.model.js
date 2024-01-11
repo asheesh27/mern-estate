4f32d59872bd47c512322ff748211b0b74d265c6
asheesh27 <ashishmathew301@gmail.com>
2024-01-11 19:08:23 +0530

    created user model

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+import { timeStamp } from "console";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: false,
+    },
+}, {timeStamp: true})
+
+const User = mongoos.model('User', userSchema);
+export default User
